6a4ea108d47ccb7172a59c0d619f3246a903c0cb
spasterz <steven.pasterz@gmail.com>
2025-09-25 09:44:15 -0700

    Finished Etch-A-Sketch
    Added RandomColor, OpacityChange, and ClearBtn

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@ console.log("hello world!");
 
 let numberGridSize;
 const gridSizeBtn = document.getElementById('grid-size-btn');
-const gridSize = 16 * 16;
-console.log(gridSize);
+const clearSquaresBtn = document.getElementById('clear-squares-btn');
+const gridSize = 16;
+// console.log(gridSize);
 const container = document.getElementById('main-container');
+const uiContainer = document.getElementById('ui-container')
 const pixel = document.createElement('div');
 const pixelSize = 20;
 let pixelArray = [];
@@ -13,8 +15,15 @@ let pixelArray = [];
 
 gridSizeBtn.addEventListener('click', pickSize);
 
+clearSquaresBtn.addEventListener('click', () => {
+    pixelArray.map(item => {
+        item.style.backgroundColor = "white";
+    })
+})
 
 function pickSize() {
+    container.replaceChildren();
+    pixelArray = [];
     const userGridSize = prompt("Please enter a value for Grid Size between 8 and 100 ");
     numberGridSize = Number(userGridSize);
 
@@ -27,29 +36,49 @@ function pickSize() {
         pickSize();
     }
 
-    console.log(numberGridSize);
+    createGrid(numberGridSize);
 }
 
-function createGrid() {
-    container.style.width = `${(Math.sqrt(gridSize) * pixelSize) + 32}px`
+function pickRandomColor() {
+    let max = 235;
+    return `rgb(${Math.floor(Math.random() * max)}, ${Math.floor(Math.random() * max)}, ${Math.floor(Math.random() * max)})`
+}
+
+function createGrid(gridSize) {
+    console.log(gridSize);
+    container.style.width = `${gridSize * pixelSize + 2}px`
+    uiContainer.style.width = container.style.width;
     console.log(container.style.width);
-    for (let i = 0; i < gridSize; i++) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
         let pixel = document.createElement('div');
-        pixel.setAttribute('style', 'width: 20px; height: 20px; border: .5px solid #eaeaea');
+        pixel.classList.add('pixel');
         pixelArray.push(pixel);
         container.appendChild(pixel);
+        // pixelArray.map(item => {
+        //     if (pixelArray.indexOf(item) < gridSize) {
+        //         item.innerText = "x"
+        //     }
+        // })
     }
+
+    etchSketch();
 }
 
-createGrid();
+createGrid(gridSize);
 
 function etchSketch() {
     pixelArray.forEach(pixel => {
         pixel.addEventListener('mouseover', () => {
             console.log("pixel hover");
-            pixel.style.backgroundColor = "red";
+            if (pixel.classList.contains('active')) {
+                if (pixel.style.opacity< 1) {
+                    pixel.style.opacity = `${Number(pixel.style.opacity) + .25}`;
+                    console.log(pixel.style.opacity)
+                }
+            } else {
+                pixel.classList.add('active');
+            }
+            pixel.style.backgroundColor = pickRandomColor();
         })
     })
 }
-
-etchSketch();
